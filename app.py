dc1334eb3130d7f2ce822427f24b8c1691ba1f71
jelleriecker <jelle@riecker.org>
2023-11-30 08:58:56 +0100

    added inspiration for errorhandeling and tried some form > dict stuff

--- app.py
+++ app.py
@@ -10,13 +10,6 @@ long_urls = {}
 short_urls = {}
 url_store = {}
 
-#error codes TODO: implement these errorcodes in process
-error400 - "Url not present"
-error404 - "Shortcode not found"
-error409 - "Shortcode already in use"
-error412 = "The provided shortcode is invalid"
-
-
 def store_url(url):
     return print(long_urls, short_urls)
 
@@ -29,25 +22,51 @@ def shorten_url(url):
         shortcode = request.form.get('shortcode')
         short_url = short_prefix + shortcode
         short_urls[url] = short_url + shortcode
-        for lurl in long_urls:
-            long_urls + shortcode
-            
         return short_url 
    
 #below is the function that will handle the request from the user
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def shortener():
     if request.method == 'POST':
-        url = request.form.get('url')
+        data = request.form.to_dict('url', 'shortcode')
+        data['shortcode'] = "url"
+        url = request.form.get('url') 
         short_url = shorten_url(url)
         short_urls[short_url] = url
+        print(data)
         return render_template('shortened-url.html', short_url=short_url)
     return render_template('shortener.html')
 
+'''
+#error codes TODO: implement these errorcodes in process
 
 
+#errorhandeling variables for html pages
+error400 - "Url not present"
+error404 - "Shortcode not found"
+error409 - "Shortcode already in use"
+error412 = "The provided shortcode is invalid"
 
-
+#Handling error 400 and displaying relevant web page
+@app.error_handler(400)
+def not_found_error(error):
+    return render_template('error400.html'),400
+ 
+#Handling error 404 and displaying relevant web page
+@app.error_handler(404)
+def internal_error(error):
+    return render_template('error404.html'),404
+ 
+#Handling error 409 and displaying relevant web page
+@app.error_handler(409)
+def not_found_error(error):
+    return render_template('error409.html'),409
+ 
+#Handling error 412 and displaying relevant web page
+@app.error_handler(412)
+def internal_error(error):
+    return render_template('error412.html'),412
+'''
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9999)
+    app.run(port=9999)
